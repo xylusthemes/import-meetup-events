71acb4399f3e7f84d5571ce89cf92a377c6503e0
Rajat Patel <prajat21@gmail.com>
2022-05-06 10:08:09 +0530

    some improvement

--- includes/class-import-meetup-events-list-table.php
+++ includes/class-import-meetup-events-list-table.php
@@ -449,8 +449,12 @@ class Import_Meetup_Events_History_List_Table extends WP_List_Table {
 		$delete_ids  = get_posts( array( 'numberposts' => 1,'fields' => 'ids', 'post_type'   => 'ime_import_history' ) );
 		if( !empty( $delete_ids ) ){
 			$wp_delete_nonce_url = esc_url( wp_nonce_url( add_query_arg( $ime_url_all_delete_args, admin_url( 'admin.php' ) ), 'ime_delete_all_history_nonce' ) );
-			$actions = '<a class="button apply" href="'.$wp_delete_nonce_url.'" onclick="return confirm(\'Warning!! Are you sure to Delete this import history? Import history will be permanatly deleted.\')">'.esc_html__( 'Clear Import History', 'import-meetup-events' ).'</a>';
-			echo $actions;
+			$confirmation_message = esc_html__( "Warning!! Are you sure to delete all these import history? Import history will be permanatly deleted.", "import-meetup-events" );
+			?>
+			<a class="button apply" href="<?php echo $wp_delete_nonce_url; ?>" onclick="return confirm('<?php echo $confirmation_message; ?>')">
+				<?php esc_html_e( 'Clear Import History', 'import-meetup-events' ); ?>
+			</a>
+			<?php
 		}
 
 		$action = array(
